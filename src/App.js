7fe3e493e271ff369d8dc8569e4ced9b4be5c368
jrizo15 <j.rizo15@yahoo.com>
2021-01-11 20:24:40 -0800

    Third commit, added form capability to type in name and job.

--- src/App.js
+++ src/App.js
@@ -1,46 +1,21 @@
-import React, { Component } from 'react'
-import Table from './Table'
+import React, { Component } from 'react';
+import Table from './Table';
+import Form from './Form';
 
 class App extends Component {
-  state = {
-    characters: [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspiring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ],
-  }
-  removeCharacter = index => {
-    const { characters } = this.state
-
-    this.setState({
-      characters: characters.filter((character, i) => {
-        return i !== index
-      }),
-    })
-  }
-  
-  render() {
-	  const { characters } = this.state;
+	state = {
+		characters: []
+	};
+render() {
+	const { characters } = this.state;
 	
-  	return (
-  		  <div className="container">
-			  <Table characterData={characters} removeCharacter={this.removeCharacter} />
-		  </div>
-	  );
-  }
+return (
+	<div className="container">
+		<Table characterData={characters} removeCharacter={this.removeCharacter} />
+		<Form />
+	</div>
+);
 }
 
+}
 export default App
